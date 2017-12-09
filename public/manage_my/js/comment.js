db1a02cd6f732e43a301b92f781d4e65fa07ef1c
boge <759076227@qq.com>
2017-12-09 17:09:47 +0800

    退出模态框

---
+++ public/manage_my/js/comment.js
@@ -0,0 +1,32 @@
+$(function () {
+    //判断员工是否登录
+    $.ajax({
+        url:'/employee/checkRootLogin',
+        success:function(backData){
+            // console.log(backData);
+            if(backData.error==400){
+                window.location.href = './login.html'
+            }else{
+
+            }
+        }
+    })
+    //点击退出注销登录
+    $('#loginture').click(function(){
+        console.log('...');
+        $.ajax({
+            url:'/employee/employeeLogout',
+            success:function(backData){
+                console.log(backData);
+                //隐藏确认框
+                $('.turelogin').modal('hide')
+                window.location.href = './login.html'
+            }
+        })
+    })
+    //点击显示侧边栏 
+    $('.main .glyphicon-align-justify').click(function () {
+        $('.slide').toggle();
+        $('.main').toggleClass('main-fullscreen');
+    })
+})
--- public/manage_my/js/index.js
+++ public/manage_my/js/index.js
@@ -1,77 +1,83 @@
-var myChart1 = echarts.init(document.getElementById('main1'));
+$(function () {
+    
+    var myChart1 = echarts.init(document.getElementById('main1'));
 
-// 指定图表的配置项和数据
-var option1 = {
-    title: {
-        text: '2017年注册人数'
-    },
-    tooltip: {},
-    legend: {
-        data: ['人数']
-    },
-    xAxis: {
-        data: ["一月", "二月", "三月", "四月", "五月", "六月"]
-    },
-    yAxis: {},
-    series: [{
-        name: '人数',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
-};
+    // 指定图表的配置项和数据
+    var option1 = {
+        title: {
+            text: '2017年注册人数'
+        },
+        tooltip: {},
+        legend: {
+            data: ['人数']
+        },
+        xAxis: {
+            data: ["一月", "二月", "三月", "四月", "五月", "六月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
 
-myChart1.setOption(option1);
-// 使用刚指定的配置项和数据显示图表。
-var myChart2 = echarts.init(document.getElementById('main2'));
+    myChart1.setOption(option1);
+    // 使用刚指定的配置项和数据显示图表。
+    var myChart2 = echarts.init(document.getElementById('main2'));
 
-option2 = {
-    title: {
-        text: '某站点用户访问来源',
-        subtext: '纯属虚构',
-        x: 'center'
-    },
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        orient: 'vertical',
-        left: 'left',
-        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-    },
-    series: [{
-        name: '访问来源',
-        type: 'pie',
-        radius: '55%',
-        center: ['50%', '60%'],
-        data: [{
-                value: 335,
-                name: '耐克'
-            },
-            {
-                value: 310,
-                name: '阿迪达斯'
-            },
-            {
-                value: 234,
-                name: '百伦'
-            },
-            {
-                value: 135,
-                name: '安踏'
-            },
-            {
-                value: 1548,
-                name: '李宁'
+    option2 = {
+        title: {
+            text: '某站点用户访问来源',
+            subtext: '纯属虚构',
+            x: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [{
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [{
+                    value: 335,
+                    name: '耐克'
+                },
+                {
+                    value: 310,
+                    name: '阿迪达斯'
+                },
+                {
+                    value: 234,
+                    name: '百伦'
+                },
+                {
+                    value: 135,
+                    name: '安踏'
+                },
+                {
+                    value: 1548,
+                    name: '李宁'
+                }
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
             }
-        ],
-        itemStyle: {
-            emphasis: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-        }
-    }]
-};
-myChart2.setOption(option2);
+        }]
+    };
+    myChart2.setOption(option2);
+
+
+
+})
