d29aff1dbbb663781e4768914162c7bda0cbb9f3
boge <759076227@qq.com>
2017-12-09 21:59:09 +0800

    页面数据渲染完成

--- public/manage_my/js/first.js
+++ public/manage_my/js/first.js
@@ -1,1 +1,30 @@
+$(function () {
+    var myPage = 1;
+    var pageSize = 5;
 
+    function getData() {
+        $.ajax({
+            url: '/category/queryTopCategoryPaging',
+            data: {
+                page: myPage,
+                pageSize: pageSize
+            },
+            success: function (backData) {
+                console.log(backData);
+                $('tbody').html(template('firstTmp', backData));
+                $("#pagintor").bootstrapPaginator({
+                    bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: myPage, //当前页
+                    totalPages: Math.ceil(backData.total / backData.size), //总页数
+                    size: "small", //设置控件的大小，mini, small, normal,large
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        myPage = page;
+                        getData();
+                    }
+                });
+            }
+        })
+    }
+    getData();
+})
